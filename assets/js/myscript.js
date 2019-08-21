47e8fb0f01415945b8dceb0422e7d431cfa34f3e
aLvinpri <alvhient@gmail.com>
2019-08-21 22:04:39 +0700

    mahasiswa model

---
+++ assets/js/myscript.js
@@ -0,0 +1,40 @@
+$(function () {
+
+    $('.tombolTambahData').on('click', function () {
+        $('#formModalLabel').html('Tambah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Tambah Data');
+        $('#nama').val('');
+        $('#nrp').val('');
+        $('#email').val('');
+        $('#jurusan').val('');
+        $('#id').val('');
+    });
+
+
+    $('.tampilModalUbah').on('click', function () {
+
+        $('#formModalLabel').html('Ubah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Ubah Data');
+        $('.modal-body form').attr('action', 'http://localhost:81/Phpmvc/public/mahasiswa/ubah'); //mengubah action form dari tambah menjadi ubah
+
+        const id = $(this).data('id');
+
+        $.ajax({
+            url: 'http://localhost:81/Phpmvc/public/mahasiswa/getUbah',
+            data: {
+                id: id
+            },
+            method: 'post',
+            dataType: 'json',
+            success: function (data) {
+                $('#nama').val(data.nama);
+                $('#nrp').val(data.nrp);
+                $('#email').val(data.email);
+                $('#jurusan').val(data.jurusan);
+                $('#id').val(data.id);
+            }
+        });
+
+    });
+
+});
