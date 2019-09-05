e99789a60b52b0dd3a7946dcefd986606ef49575
aLvinpri <alvhient@gmail.com>
2019-09-05 14:45:29 +0700

    vid 14 12:35

---
+++ application/views/peoples/index.php
@@ -0,0 +1,24 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-10">
+      <h3 class="mt-3">List Of People</h3>
+      <div class="table">
+        <div class="thead">
+          <div class="tr">
+            <div class="th">#</div>
+            <div class="th">Name</div>
+            <div class="th">Email</div>
+            <div class="th">Action</div>
+          </div>
+        </div>
+        <div class="tbody">
+          <div class="tr">
+            <div class="td"></div>
+            <div class="td"></div>
+            <div class="td"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
