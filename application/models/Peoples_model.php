f4b1759829f347ad8d7dc086d2e08c11589a33b7
aLvinpri <alvhient@gmail.com>
2019-09-10 22:25:17 +0700

    vid 15 searching done

--- application/models/Peoples_model.php
+++ application/models/Peoples_model.php
@@ -3,8 +3,12 @@
 class Peoples_model extends CI_Model
 {
 
-  public function getAll($table, $limit, $start)
+  public function getAll($table, $limit, $start, $keyword)
   {
+    if ($keyword) {
+      $this->db->like('name', $keyword);
+      $this->db->or_like('email', $keyword);
+    }
     return $this->db->get($table, $limit, $start)->result_array();
   }
 
--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -1,7 +1,20 @@
 <div class="container">
+  <h3 class="mt-3">List Of People</h3>
+  <div class="row">
+    <div class="col-md-6">
+      <form action="<?= base_url('peoples'); ?>" method="POST">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Search Keyword..." name="keyword" autocomplete="off" autofocus>
+          <div class="input-group-append">
+            <input class="btn btn-primary" type="submit" name="submit">
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
   <div class="row">
     <div class="col-md-10">
-      <h3>List Of People</h3>
+      <h5>Result : <?= $total_rows; ?></h5>
       <table class="table table-striped">
         <thead>
           <tr>
@@ -13,6 +26,16 @@
         </thead>
         <tbody>
           <?php
+          if (empty($peoples)) :
+            echo '
+            <tr>
+              <td colspan="4">
+                <div class="alert alert-danger text-center mt-2" role="alert">
+                  <h4>Data ' . $keyword . ' Not Found !</h4>
+                </div>
+              </td>
+            </tr>';
+          endif;
           $i = 1;
           foreach ($peoples as $people) : ?>
             <tr>
